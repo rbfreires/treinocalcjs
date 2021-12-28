363c7e455d061fefd5385baebdf1b02d92971906
Rafael <rbfreires@gmail.com>
2021-12-28 12:00:47 -0300

    melhorias

--- Js/script.js
+++ Js/script.js
@@ -1,19 +1,77 @@
+// declarações globais de variáveis
+const custofrete = document.querySelector("#custofrete");
+const mlucro = document.querySelector("#margemlucro");
+const txseguro = document.querySelector("#taxaseguro");
+const vLucro = document.querySelector("#lucro");
+const vSeguro = document.querySelector("#valorseguro");
+const valCarga = document.querySelector("#valorcarga");
+const tximposto = document.querySelector("#taxaimposto");
+const valPedagio = document.querySelector("#custopedagio");
+const valCargaDescarga = document.querySelector("#cargadescarga");
+const processa = document.querySelector("#calculo");
+
+// função clucro
 function clucro() {
-  const custofrete = document.querySelector("#custofrete");
-  const mlucro = document.querySelector("#margemlucro");
+  if (custofrete.value == "") {
+    alert("Informar o custo do frete.");
+    custofrete.focus();
+    return;
+  }
   const valorCusto = custofrete.value;
   const valorMlucro = mlucro.value;
   const result = (valorCusto * valorMlucro) / 100;
-  const vLucro = "R$" + result.toFixed(2);
+  const vLucro = result;
   document.querySelector("#lucro").value = vLucro;
 }
 
+// função  cseguro
 function cseguro() {
-  const txseguro = document.querySelector("#taxaseguro");
-  const valCarga = document.querySelector("#valorcarga");
   const taxaSeguro = txseguro.value;
   const valorCarga = valCarga.value;
   const calcseguro = (valorCarga * taxaSeguro) / 100;
-  const vSeguro = "R$" + calcseguro.toFixed(2);
+  const vSeguro = calcseguro;
   document.querySelector("#valorseguro").value = vSeguro;
+}
+
+// função cimposto
+function cimposto() {
+  if (custofrete.value == "") {
+    alert("Informar o custo do frete.");
+    custofrete.focus();
+    return;
+  }
+
+  if (valPedagio.value == "") {
+    valPedagio.value = 0;
+  }
+  if (valCargaDescarga.value == "") {
+    valCargaDescarga.value = 0;
+  }
+
+  const VcustoFrete = custofrete.value;
+  const Vlucro = vLucro.value;
+  const Vseguro = vSeguro.value;
+  const Vpedagio = valPedagio.value;
+  const VcargaEdescarga = valCargaDescarga.value;
+  const TXimposto = tximposto.value;
+
+  const soma1 =
+    parseFloat(VcustoFrete) +
+    parseFloat(Vlucro) +
+    parseFloat(Vpedagio) +
+    parseFloat(VcargaEdescarga) +
+    parseFloat(Vseguro);
+
+  const Vimposto = (soma1 * TXimposto) / 100;
+
+  const TotServ = parseFloat(soma1) + parseFloat(Vimposto);
+
+  document.querySelector("#valorimposto").value = Vimposto.toLocaleString(
+    "pt-br",
+    { style: "currency", currency: "BRL" }
+  );
+  document.querySelector("#totalservico").value = TotServ.toLocaleString(
+    "pt-br",
+    { style: "currency", currency: "BRL" }
+  );
 }
--- index.php
+++ index.php
@@ -1,61 +1,83 @@
 <?php
 
-include('header.php');
-include('footer.php');
+include('includes/header.php');
+
 ?>
 
 <div class="content container-fluid">
     <div class="container box">
         <h1 class="tituloform">Cálculo de Fretes</h1>
-        <form action="" class="principal row g-3 form-control d-flex justify-content-between">
-            <div class="mb-3 col-sm-4">
-                <label for="custofrete" class="form-label">Custo Frete</label>
-                <input type="text" class="custofrete form-control form-control-sm" name="custofrete" id="custofrete" aria-describedby="helpId" placeholder="Informe o custo do frete..." onkeyup="formatMoeda()" required>
+        <form class="principal row g-3 form-control d-flex justify-content-between">
+            <div class="mb-3 col-sm-2">
+                <label for="data" class="form-label">Origem da Coleta</label>
+                <input type="date" class="form-control form-control-sm" name="data" id="data" aria-describedby="helpId" placeholder="Informe a data...">
             </div>
-            <div class="mb-3 col-sm-4">
-                <label for="margemlucro" class="form-label">Margem de Lucro</label>
-                <input type="text" class="form-control form-control-sm" name="margemlucro" id="margemlucro" aria-describedby="helpId" placeholder="Informe a margem de lucro..." onkeyup="clucro()" required>
+            <div class="mb-3 col-sm-5">
+                <label for="origem" class="form-label">Origem da Coleta</label>
+                <input type="text" class="form-control form-control-sm" name="origem" id="origem" aria-describedby="helpId" placeholder="Informe a origem da coleta...">
             </div>
-            <div class="mb-3 col-sm-4">
-                <label for="lucro" class="form-label">Lucro</label>
-                <input type="text" class="form-control form-control-sm" name="lucro" id="lucro" aria-describedby="helpId" placeholder="Valor do lucro..." readonly>
+            <div class="mb-3 col-sm-5">
+                <label for="destino" class="form-label">Destino da Coleta</label>
+                <input type="text" class="form-control form-control-sm" name="destino" id="destino" aria-describedby="helpId" placeholder="Informe o destino da entrega...">
             </div>
-            <div class="mb-3 col-sm-4">
-                <label for="custopedagio" class="form-label">Pedágios</label>
-                <input type="text" class="form-control form-control-sm" name="custopedagio" id="custopedagio" aria-describedby="helpId" placeholder="Informe o custo com pedágios...">
+            <div class="mb-3 col-sm-6">
+                <label for="carreteiro" class="form-label">Motorista (Carreteiro)</label>
+                <input type="text" class="form-control form-control-sm" name="carreteiro" id="carreteiro" aria-describedby="helpId" placeholder="Informe o motorista que cotou o frete...">
             </div>
-            <div class="mb-3 col-sm-4">
-                <label for="cargadescarga" class="form-label">Carga/Descarga</label>
-                <input type="text" class="form-control form-control-sm" name="cargadescarga" id="cargadescarga" aria-describedby="helpId" placeholder="Informe o custo da carga ou descarga...">
-            </div>
-            <div class="mb-3 col-sm-4">
-                <label for="valorcarga" class="form-label">Valor da mercadoria</label>
-                <input type="text" class="form-control form-control-sm" name="valorcarga" id="valorcarga" aria-describedby="helpId" placeholder="Informe o valor da mercadoria..." required>
-            </div>
-            <div class="mb-3 col-sm-4">
-                <label for="taxaseguro" class="form-label">Taxa de Seguro</label>
-                <input type="text" class="form-control form-control-sm" name="taxaseguro" id="taxaseguro" aria-describedby="helpId" placeholder="Informe a taxa de seguro..." onkeyup="cseguro()" required>
-            </div>
-            <div class="mb-3 col-sm-4">
-                <label for="valorseguro" class="form-label">Valor do Seguro</label>
-                <input type="text" class="form-control form-control-sm" name="valorseguro" id="valorseguro" aria-describedby="helpId" placeholder="Valor do seguro" readonly>
-            </div>
-            <div class="mb-3 col-sm-4">
-                <label for="taxaimposto" class="form-label">Alíquota Imposto</label>
-                <input type="text" class="form-control form-control-sm" name="taxaimposto" id="taxaimposto" aria-describedby="helpId" placeholder="Informe a taxa de imposto..." required>
-            </div>
-            <div class="mb-3 col-sm-4">
-                <label for="valorimposto" class="form-label">Valor Imposto</label>
-                <input type="text" class="form-control form-control-sm" name="valorimposto" id="valorimposto" aria-describedby="helpId" placeholder="Valor imposto..." readonly>
-            </div>
-            <div class="mb-3 col-sm-4">
-                <label for="totalservico" class="form-label">Total dos Serviços</label>
-                <input type="text" class="form-control form-control-sm" name="totalservico" id="totalservico" aria-describedby="helpId" placeholder="Valor imposto..." readonly>
-            </div>
-            <div class="botao">
-                <button class="btn calc btn-outline-primary" id="calculo">Calcular Frete</button>
-                <button class="btn btn-outline-primary">Limpar</button>
-            </div>
-        </form>
+            <form class="principal row g-3 form-control d-flex justify-content-between">
+                <div class="mb-3 col-sm-2">
+                    <label for="custofrete" class="form-label">Custo Frete</label>
+                    <input type="text" class="custofrete form-control form-control-sm" name="custofrete" id="custofrete" aria-describedby="helpId" placeholder="Informe o custo do frete...">
+                </div>
+                <div class="mb-3 col-sm-2">
+                    <label for="margemlucro" class="form-label">Margem de Lucro</label>
+                    <input type="text" class="form-control form-control-sm" name="margemlucro" id="margemlucro" aria-describedby="helpId" placeholder="Informe a margem de lucro..." onkeyup="clucro()" required>
+                </div>
+                <div class="mb-3 col-sm-2">
+                    <label for="lucro" class="form-label">Lucro</label>
+                    <input type="text" class="form-control form-control-sm" name="lucro" id="lucro" aria-describedby="helpId" placeholder="Valor do lucro..." readonly>
+                </div>
+                <div class="mb-3 col-sm-2">
+                    <label for="custopedagio" class="form-label">Pedágios</label>
+                    <input type="text" class="form-control form-control-sm" name="custopedagio" id="custopedagio" aria-describedby="helpId" placeholder="Informe os pedágios...">
+                </div>
+                <div class="mb-3 col-sm-2">
+                    <label for="cargadescarga" class="form-label">Carga/Descarga</label>
+                    <input type="text" class="form-control form-control-sm" name="cargadescarga" id="cargadescarga" aria-describedby="helpId" placeholder="Informe a carga/descarga...">
+                </div>
+                <div class="mb-3 col-sm-2">
+                    <label for="valorcarga" class="form-label">Valor da mercadoria</label>
+                    <input type="text" class="form-control form-control-sm" name="valorcarga" id="valorcarga" aria-describedby="helpId" placeholder="Informe o valor da mercadoria..." required>
+                </div>
+                <div class="mb-3 col-sm-2">
+                    <label for="taxaseguro" class="form-label">Taxa de Seguro</label>
+                    <input type="text" class="form-control form-control-sm" name="taxaseguro" id="taxaseguro" aria-describedby="helpId" placeholder="Informe a taxa de seguro..." onkeyup="cseguro()" required>
+                </div>
+                <div class="mb-3 col-sm-4">
+                    <label for="valorseguro" class="form-label">Valor do Seguro</label>
+                    <input type="text" class="form-control form-control-sm" name="valorseguro" id="valorseguro" aria-describedby="helpId" placeholder="Valor do seguro" readonly>
+                </div>
+                <div class="mb-3 col-sm-4">
+                    <label for="taxaimposto" class="form-label">Alíquota Imposto</label>
+                    <input type="text" class="form-control form-control-sm" name="taxaimposto" id="taxaimposto" aria-describedby="helpId" placeholder="Informe a taxa de imposto..." required>
+                </div>
+                <div class="mb-3 col-sm-4">
+                    <label for="valorimposto" class="form-label">Valor Imposto</label>
+                    <input type="text" class="form-control form-control-sm" name="valorimposto" id="valorimposto" aria-describedby="helpId" placeholder="Valor imposto..." readonly>
+                </div>
+                <div class="mb-3 col-sm-4">
+                    <label for="totalservico" class="form-label">Total dos Serviços</label>
+                    <input type="text" class="form-control form-control-sm" name="totalservico" id="totalservico" aria-describedby="helpId" placeholder="Valor total..." readonly>
+                </div>
+                <div class="botao">
+                    <button type="button" class="btn calc btn-outline-primary" id="calculo" onclick="cimposto()">Calcular Frete</button>
+                    <button class="btn btn-outline-primary">Limpar</button>
+                </div>
+            </form>
     </div>
 </div>
+<?php
+
+include('includes/footer.php');
+
+?>
